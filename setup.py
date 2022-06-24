42ead68077678bea350629da2107b7cba65a3928
Oliver <oliver@debian-BULLSEYE-live-builder-AMD64>
2022-06-24 17:49:22 -0500

    Changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pytentiostat',
-    version='0.0.1',
+    version='0.0.2',
     author='Oliver Rodriguez',
     author_email='oliverrz@illinois.edu',
     packages=find_packages('src'),
--- src/pytentiostat/__init__.py
+++ src/pytentiostat/__init__.py
@@ -6,7 +6,7 @@ pytentiostat library
 from .potentiostat import *
 from .file import *
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __author__ = 'Oliver Rodriguez'
 
 __all__ = ['pytentiostat', 'chi760e', 'file']
