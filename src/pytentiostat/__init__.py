98df3386eeec4ce43efcca0656f20b89a76552c1
Oliver <oliverrz@illinois.edu>
2022-06-29 15:22:36 -0500

    Removed routines

--- src/pytentiostat/__init__.py
+++ src/pytentiostat/__init__.py
@@ -5,10 +5,9 @@ pytentiostat library
 
 from . import potentiostat
 from . import load_data
-from . import routines
 
 __version__ = "0.0.4"
 __author__ = 'Oliver Rodriguez'
 
 # modules to import when user does 'from pytentiostat import *':
-__all__ = ['potentiostat', 'load_data', 'routines']
+__all__ = ['potentiostat', 'load_data']
