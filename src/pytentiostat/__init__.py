943c4df419942a5c1e9bead976050b098f1c9080
Oliver <oliverrz@illinois.edu>
2022-06-29 14:33:49 -0500

    Added routines module

--- src/pytentiostat/__init__.py
+++ src/pytentiostat/__init__.py
@@ -3,13 +3,12 @@ pytentiostat library
 
 """
 
-#from .potentiostat import *
-#from .load_data import *
 from . import potentiostat
 from . import load_data
+from . import routines
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __author__ = 'Oliver Rodriguez'
 
 # modules to import when user does 'from pytentiostat import *':
-__all__ = ['potentiostat', 'load_data' ]
+__all__ = ['potentiostat', 'load_data', 'routines']
