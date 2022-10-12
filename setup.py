5bd9f03cbb2cb79dbaa24fabf8c11ecec0c635da
Oliver Rodriguez <oliver.rdz@gmail.com>
2022-10-12 11:03:46 -0500

    changed import to pypotato

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pypotato',
-    version='1.1.0',
+    version='1.1.1',
     author='Oliver Rodriguez',
     author_email='oliver.rdz@softpotato.xyz',
     packages=find_packages('src'),
