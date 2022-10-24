9d9f22b4c8e5fda49bea500a2c7a6571d862e82a
Oliver Rodriguez <oliver.rdz@gmail.com>
2022-10-24 12:31:23 -0500

    Added example

---
+++ examples/CV.py
@@ -0,0 +1,34 @@
+from pypotato import *
+
+# Select the potentiostat model to use:
+model = 'chi760e'
+# Path to the chi software, including extension .exe
+path = 'C://Users/oliver/chi/chi760e.exe'
+# Folder where to save the data, it needs to be created previously
+folder = 'C://Users/oliver/data'
+# Initialization:
+setup = potentiostat.Setup(model, path, folder)
+
+
+# Experimental parameters:
+Eini = -0.5     # V, initial potential
+Ev1 = 0.5       # V, first vertex potential
+Ev2 = -0.5      # V, second vertex potential
+Efin = -0.5     # V, final potential
+sr = 0.1        # V/s, scan rate
+dE = 0.001      # V, potential increment
+nSweeps = 2     # number of sweeps
+sens = 1e-6     # A/V, current sensitivity
+E2 = 0.5        # V, potential of the second working electrode
+sens2 = 1e-6    # A/V, current sensitivity of the second working electrode
+fileName = 'CV' # base file name for data file
+header = 'CV'   # header for data file
+
+# initialize experiment:
+cv = potentiostat.CV(Eini, Ev1,Ev2, Efin, sr, dE, nSweeps, sens, fileName, header)
+# Include second working electrode in bipotentiostat mode.
+# Comment or delete the next line to remove bipot mode.
+cv.bipot(E2,sens2)
+# Run experiment:
+cv.run()
+
