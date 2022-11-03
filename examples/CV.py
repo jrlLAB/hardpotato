8f51d10fcc4efed1f57ae82f227ef280b260b606
oliver <oliver.rdz@softpotato.xyz>
2022-11-03 14:54:45 -0700

    Tested

--- examples/CV.py
+++ examples/CV.py
@@ -1,14 +1,16 @@
-from pypotato import *
+import pypotato as pp
+import softpotato as sp
 
 # Select the potentiostat model to use:
-model = 'chi760e'
 #model = 'chi760e'
-# Path to the chi software, including extension .exe
-path = 'C:/Users/jrl/Desktop/CHI/chi760e/chi760e.exe'
+model = 'chi1205b'
+#model = 'emstatpico'
+# Path to the chi software, including extension .exe. Negletected by emstatpico
+path = 'C:/Users/oliverrz/Desktop/CHI/chi1205b_mini2/chi1205b.exe'
 # Folder where to save the data, it needs to be created previously
-folder = 'C:/Users/jrl/echem/Data'
+folder = 'data'
 # Initialization:
-potentiostat.Setup(model, path, folder)
+pp.potentiostat.Setup(model, path, folder)
 
 
 # Experimental parameters:
@@ -16,7 +18,7 @@ Eini = -0.5     # V, initial potential
 Ev1 = 0.5       # V, first vertex potential
 Ev2 = -0.5      # V, second vertex potential
 Efin = -0.5     # V, final potential
-sr = 1          # V/s, scan rate
+sr = 1        # V/s, scan rate
 dE = 0.001      # V, potential increment
 nSweeps = 2     # number of sweeps
 sens = 1e-6     # A/V, current sensitivity
@@ -26,9 +28,15 @@ fileName = 'CV' # base file name for data file
 header = 'CV'   # header for data file
 
 # initialize experiment:
-cv = potentiostat.CV(Eini, Ev1,Ev2, Efin, sr, dE, nSweeps, sens, fileName, header)
-# Include second working electrode in bipotentiostat mode.
-# Comment or delete the next line to remove bipot mode.
-#cv.bipot(E2,sens2)
+cv = pp.potentiostat.CV(Eini, Ev1,Ev2, Efin, sr, dE, nSweeps, sens, fileName, header)
 # Run experiment:
 cv.run()
+
+# Load recently acquired data
+data = pp.load_data.CV(fileName +'.txt', folder, model)
+i = data.i
+E = data.E
+
+# Plot CV with softpotato
+sp.plotting.plot(E, i, fig=1, show=1)
+
