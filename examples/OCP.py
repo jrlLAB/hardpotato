67def6a3e83dd67a22d54331d1bcc9540a40e82c
oliver <oliver.rdz@softpotato.xyz>
2022-11-03 15:44:47 -0700

    Tested

--- examples/OCP.py
+++ examples/OCP.py
@@ -1,17 +1,19 @@
-from pypotato import *
+import pypotato as pp
+import softpotato as sp
 
 # Select the potentiostat model to use:
-model = 'chi760e'
+#model = 'chi760e'
+model = 'chi1205b'
+#model = 'emstatpico'
 
 # Path to the chi software, including extension .exe
-path = 'C:/Users/jrl/Desktop/CHI/chi760e/chi760e.exe'
+path = 'C:/Users/oliverrz/Desktop/CHI/chi1205b_mini2/chi1205b.exe'
 
 # Folder where to save the data, it needs to be created previously
-folder = 'C:/Users/jrl/echem/Data'
+folder = 'C:/Users/oliverrz/Desktop/data'
 
 # Initialization:
-potentiostat.Setup(model, path, folder)
-
+pp.potentiostat.Setup(model, path, folder)
 
 # Experimental parameters:
 ttot = 1        # s, total time
@@ -20,6 +22,15 @@ fileName = 'OCP' # base file name for data file
 header = 'OCP'   # header for data file
 
 # initialize experiment:
-ocp = potentiostat.OCP(ttot, dt, fileName, header)
+ocp = pp.potentiostat.OCP(ttot, dt, fileName, header)
 # Run experiment:
 ocp.run()
+
+# Load recently acquired data
+data = pp.load_data.OCP(fileName +'.txt', folder, model)
+E = data.E
+t = data.t
+
+# Plot CV with softpotato
+sp.plotting.plot(t, E, xlab='$t$ / s', ylab='$E$ / V', fig=1, show=1)
+
