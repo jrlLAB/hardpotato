1714104d1c57fc5125f94068cc7066c6a94fc0c1
Oliver Rodriguez <oliver.rdz@gmail.com>
2023-01-04 13:37:47 -0600

    Version 1.3.2

--- src/pypotato/__init__.py
+++ src/pypotato/__init__.py
@@ -6,8 +6,8 @@ pypotato library
 from . import potentiostat
 from . import load_data
 
-__version__ = "1.2.3"
+__version__ = "1.3.2"
 __author__ = 'Oliver Rodriguez'
 
-# modules to import when user does 'from pytentiostat import *':
+# modules to import when user does 'from pypotato import *':
 __all__ = ['potentiostat', 'load_data']
